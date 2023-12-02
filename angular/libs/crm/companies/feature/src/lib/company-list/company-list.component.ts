b13e0de5dfc4d038deb17756b3fe3cf16e53f4f6
Steff Beckers <steff@steffbeckers.eu>
2023-12-02 20:10:42 +0100

    Company list component and companies store added

---
+++ angular/libs/crm/companies/feature/src/lib/company-list/company-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sb-company-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './company-list.component.html',
+  styleUrl: './company-list.component.scss',
+})
+export class CompanyListComponent {}
