2e9a52e1e55122928acdc0bc1f36d6be7179c876
Steff Beckers <steff@steffbeckers.eu>
2023-12-17 19:31:58 +0100

    Testing updates

--- angular/libs/crm/companies/feature/src/lib/company-list/company-list.component.spec.ts
+++ angular/libs/crm/companies/feature/src/lib/company-list/company-list.component.spec.ts
@@ -1,13 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CompanyListComponent } from './company-list.component';
+import { CompaniesService } from '@steffbeckers/crm/data-access/proxy/crm/companies';
 
 describe('CompanyListComponent', () => {
   let component: CompanyListComponent;
   let fixture: ComponentFixture<CompanyListComponent>;
+  const companiesServiceMock = {
+    getList: jest.fn(),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CompanyListComponent],
+      providers: [
+        {
+          provide: CompaniesService,
+          useValue: companiesServiceMock,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CompanyListComponent);
