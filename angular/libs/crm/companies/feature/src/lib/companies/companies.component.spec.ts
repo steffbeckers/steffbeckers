ed64b3757a346331364bc156fe23962aced04889
Steff Beckers <steff@steffbeckers.eu>
2023-12-23 11:50:10 +0100

    Updates for testing

--- angular/libs/crm/companies/feature/src/lib/companies/companies.component.spec.ts
+++ angular/libs/crm/companies/feature/src/lib/companies/companies.component.spec.ts
@@ -1,17 +1,32 @@
+import { CoreTestingModule } from '@abp/ng.core/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CompaniesComponent } from './companies.component';
 import { CompaniesService } from '@steffbeckers/crm/data-access';
+import { BehaviorSubject } from 'rxjs';
 
 describe('CompaniesComponent', () => {
   let component: CompaniesComponent;
   let fixture: ComponentFixture<CompaniesComponent>;
   const companiesServiceMock = {
-    getList: jest.fn(),
+    getList: () =>
+      new BehaviorSubject({
+        items: [
+          {
+            id: '1',
+            name: 'Company 1',
+          },
+          {
+            id: '2',
+            name: 'Company 2',
+          },
+        ],
+        totalCount: 2,
+      }),
   };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CompaniesComponent],
+      imports: [CoreTestingModule.withConfig(), CompaniesComponent],
       providers: [
         {
           provide: CompaniesService,
