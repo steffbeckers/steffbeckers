a84168e62a116eba3d6fd0a709f0ba180f6b8d4a
Steff Beckers <s.beckers@fuzed.app>
2024-05-26 10:46:14 +0200

    Testing fixes

--- angular/libs/shared/utils/testing/src/lib/testing.module.ts
+++ angular/libs/shared/utils/testing/src/lib/testing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CoreTestingModule as AbpCoreTestingModule } from '@abp/ng.core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { providePageTitleConfig } from '@steffbeckers/shared/utils/page-title';
 
 @NgModule({
   imports: [AbpCoreTestingModule.withConfig(), RouterTestingModule],
   exports: [AbpCoreTestingModule, RouterTestingModule],
+  providers: [providePageTitleConfig()],
 })
 export class TestingModule {}
