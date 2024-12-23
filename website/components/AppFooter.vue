b7737b54df4670e9b083277d22c1a7ff5da5b9d2
Steff Beckers <steff@steffbeckers.eu>
2024-12-23 12:39:34 +0100

    Responsive updates

--- website/components/AppFooter.vue
+++ website/components/AppFooter.vue
@@ -3,7 +3,9 @@ const runtimeConfig = useRuntimeConfig();
 </script>
 
 <template>
-  <div class="flex justify-between gap-4 max-w-4xl mx-auto px-4 py-4 w-full text-sm">
+  <div
+    class="flex flex-col sm:flex-row items-center sm:justify-between gap-2 max-w-4xl mx-auto px-4 py-4 w-full text-sm"
+  >
     <div>&copy; {{ new Date().getFullYear() }} - Steff Beckers</div>
     <div>Last updated on {{ formatDate(runtimeConfig.public.lastUpdatedOn) }}</div>
   </div>
