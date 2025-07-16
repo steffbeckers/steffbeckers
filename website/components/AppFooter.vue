a299f211c0b3f29905231bbd58d869b7c4543f51
Steff Beckers <steff@steffbeckers.eu>
2025-07-16 07:57:41 +0200

    Footer updated

--- website/components/AppFooter.vue
+++ website/components/AppFooter.vue
@@ -4,9 +4,9 @@ const runtimeConfig = useRuntimeConfig();
 
 <template>
   <div
-    class="flex flex-col sm:flex-row items-center sm:justify-between gap-2 max-w-4xl mx-auto px-4 py-4 w-full text-sm"
+    class="flex flex-col sm:flex-row items-center sm:justify-between gap-2 max-w-4xl mx-auto px-4 py-4 w-full text-xs"
   >
-    <div>&copy; {{ new Date().getFullYear() }} - Steff Beckers</div>
+    <div>&copy; {{ new Date().getFullYear() }} Steff Beckers. {{ $t("AllRightsReserved") }}.</div>
     <div>{{ $t("LastUpdatedOn") }} {{ formatDateTime(runtimeConfig.public.lastUpdatedOn) }}</div>
   </div>
 </template>
